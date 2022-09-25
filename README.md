4f9d7c0c9b4592a43061c195cf7fab1462009787
André Aragão <business@andreribeiroaragao.com>
2022-09-25 02:16:00 +0100

    Retrieve data from OpenSea

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ TO-DO
 
 Please refer to [Reddit's r/CollectibleAvatars FAQ](https://www.reddit.com/r/CollectibleAvatars/comments/x0tx59/hey_what_is_this_thing_collectible_avatar_faqs/) - I couldn't have written it better.
 
-### Why does not follow X JavaScript/Node/Express/* development standard paradigm?
+### Why does the project not follow X JavaScript/Node/Express/* development standard paradigm?
 
 I'm not a JavaScript/Node/Express/* developer - my day-to-day activities are focused on Java and Spring.
+
 If you have any suggestion drop me a message :)
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var axios = require('axios');
+
+router.get('/', async function (req, res, next) {
+  const collectionsNameInOpenSeaStandard = [
+    "aylia-x-reddit-collectible-avatars",
+    "foustlings-x-reddit-collectible-avatars",
+    "the-senses-x-reddit-collectible-avatars",
+    "growl-gang-x-reddit-collectible-avatars",
+    "natsukashii-x-reddit-collectible-avatars",
+    "cute-snacks-x-reddit-collectible-avatars",
+    "enlightenment-x-reddit-collectible-avatars",
+    "peculiar-gang-x-reddit-collectible-avatars",
+    "5-boro-bodega-x-reddit-collectible-avatars",
+    "joy-girls-club-x-reddit-collectible-avatars",
+    "old-school-cool-x-reddit-collectible-avatars",
+    "lightspeed-lads-x-reddit-collectible-avatars",
+    "avatar-rock-out-x-reddit-collectible-avatars",
+    "bites-of-brazil-x-reddit-collectible-avatars",
+    "the-minds-eyes-x-reddit-collectible-avatars",
+    "doodle-collection-x-reddit-collectible-avatars",
+    "gettin-groovy-x-reddit-collectible-avatars",
+    "celestial-assembly-x-reddit-collectible-avatars",
+  ];
+
+  const collectionsName = [];
+
+  populateCollectionsNameBasedOnOpenSeaStardard(collectionsName, collectionsNameInOpenSeaStandard);
+
+  var collectionsStats = {};
+
+  for (let collectionNumber = 0; collectionNumber < collectionsNameInOpenSeaStandard.length; collectionNumber++) {
+    const collectionNameInOpenSeaStandard = collectionsNameInOpenSeaStandard[collectionNumber];
+    let requestUrl = "https://api.opensea.io/api/v1/collection/" + collectionNameInOpenSeaStandard + "/stats";
+
+    const res = await axios({
+      method: 'get',
+      url: requestUrl,
+    })
+      .catch(err => console.error(err));
+    
+    const collectionStats = res.data.stats;
+    collectionsStats[collectionsName[collectionNumber]] = collectionStats;
+  }
+
+  console.log(collectionsStats);
+
+  res.render('index', { title: 'Reddit Collectible Avatars Statistics Project' });
 });
+
+function convertCollectionNameInOpenSeaStandardToCollectionName(openSeaCollectionName) {
+  return openSeaCollectionName.split("-x-")[0];
+}
+
+function populateCollectionsNameBasedOnOpenSeaStardard(collectionsName, collectionsNameInOpenSeaStandard) {
+  for (let collectionNumber = 0; collectionNumber < collectionsNameInOpenSeaStandard.length; collectionNumber++) {
+    collectionsName.push(convertCollectionNameInOpenSeaStandardToCollectionName(collectionsNameInOpenSeaStandard[collectionNumber]));
+  }
+}
 
 module.exports = router;
